7027574d5882c4eebcf9bca558c9e8b9782c65a2
GraineDePomme <Guillaume.Sarisson@gmail.com>
2025-09-26 08:11:05 +0200

    Hello, world!

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+
+    name: "ScienceKitTemplate",
+
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
+        .package(url: "https://github.com/GraineDePomme/ScienceKit", from: "1.0.0")
+    ],
+
+    targets: [
+        .executableTarget(
+            name: "ScienceKitTemplate",
+            dependencies: [
+                .product(name: "Numerics", package: "swift-numerics"),
+                .product(name: "ScienceKit", package: "ScienceKit")
+            ]
+        )
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,5 @@
+import Foundation
+import Numerics
+import ScienceKit
+
+print("Hello, world!")
