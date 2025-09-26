f0c3a71bf575f0a545342039aaf09fe3d1dbd676
GraineDePomme <Guillaume.Sarisson@gmail.com>
2025-09-26 18:01:57 +0200

    Fixing things

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,11 +2,6 @@ import Foundation
 import Numerics
 import ScienceKit
 
-// Physical constants exist with different level of precision from Float16 to Float64
-print("The Rydberg constant as a Float16 : \(Float16.rydbergConstant)")
-print("The Rydberg constant as a Float32 : \(Float32.rydbergConstant)")
-print("The Rydberg constant as a Float64 : \(Float64.rydbergConstant)")
-
 let R: Double = Double.rydbergConstant
 
 for m in 1...3 {
